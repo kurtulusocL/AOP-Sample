3b2e8daf3c7df3d67a9219d69e59c1388b8b159f
Kurtuluş Öcal <78833568+kurtulusocL@users.noreply.github.com>
2022-02-28 22:40:08 +0300

    Update City.cs

--- Entities/Concrete/City.cs
+++ Entities/Concrete/City.cs
@@ -10,6 +10,6 @@ namespace Entities.Concrete
     {
          public int Id { get; set; }
          public string Name { get; set; }
-         public int CityId { get; set; }
+         public int CountryId { get; set; }
     }
 }
